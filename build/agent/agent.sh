d24573d84e3727a8d006e13fc73e766c05cd81b2
farmer-hutao <farmer.hutao@outlook.com>
2019-04-30 14:25:26 +0800

    update agent.service

--- build/agent/agent.sh
+++ build/agent/agent.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 sudo cp agent /usr/local/bin/agent
 sudo cp ./agent.service /usr/lib/systemd/system/agent.service
-sudo systemctl enable agent.service
+
 sudo systemctl daemon-reload
+sudo systemctl enable agent.service
 sudo systemctl start agent.service
