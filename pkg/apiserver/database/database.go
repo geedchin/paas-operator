29135252fbeeae0044c7112459577f78ae51d694
胡涛 <farmer.hutao@outlook.com>
2019-05-05 13:48:09 +0800

    Apiserver (#14)
    * add dblist
    
    * add build
    
    * tyz: 新增rest客户端功能、ssh客户端功能
    
    * add ssh

--- pkg/apiserver/database/database.go
+++ pkg/apiserver/database/database.go
@@ -1,6 +1,9 @@
 package database
 
-import "github.com/kataras/iris"
+import (
+	"fmt"
+	"github.com/kataras/iris"
+)
 
 var DatabaseList = make(map[string]Database, 0)
 
@@ -29,7 +32,28 @@ type Database struct {
 }
 
 func (d *Database) Create(ctx iris.Context) {
+	var dataBase Database
 
+	if err := ctx.ReadJSON(&dataBase); err != nil {
+		fmt.Println("invalid json from the request body !")
+		ctx.StatusCode(iris.StatusBadRequest)
+		return
+	}
+
+	//if err := upload_agent(dataBase.Host[0].Username,
+	//	dataBase.Host[0].Password, dataBase.Host[0].IP); err != nil {
+	//	fmt.Printf("upload agent to remote host(%s) failed ", dataBase.Host[0].IP)
+	//	ctx.StatusCode(iris.StatusInternalServerError)
+	//	return
+	//}
+
+	// 1. validate ssh connection
+
+	// 2. distribute agent
+
+	// 3. call api - install
+
+	// 4.
 }
 
 // action can be [start / stop / install / restart / uninstall]
---
+++ pkg/apiserver/utils/ssh_client.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/pkg/sftp"
+	"os"
+)
+
+type SSHClient struct {
+	sftpClient *sftp.Client
+	session    *ssh.Session
+}
+
+func (s *SSHClient) connect() {
+
+}
+
+func (s *SSHClient) run_command(command string) error {
+	return nil
+}
+
+func (s *SSHClient) upload_agent(localFilePath, remoteFilePath string) error {
+	srcFile, err := os.Open(localFilePath)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := s.sftpClient.Create(remoteFilePath)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		n, _ := srcFile.Read(buf)
+		if n == 0 {
+			break
+		}
+		dstFile.Write(buf)
+	}
+
+	fmt.Println("copy file to remote server finished")
+
+	return nil
+}
